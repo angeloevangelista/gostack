ee61aa96a764d7092cfe544f7f40651ce92659af
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-14 19:38:58 -0300

    Tipando objetos e vetores

--- conceitos-dev/typescript/src/routes.ts
+++ conceitos-dev/typescript/src/routes.ts
@@ -1,5 +1,26 @@
 import { Response, Request } from 'express';
+import createUser from './services/CreateUser';
+
+// string, number, boolean, object, Array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    name: 'Angelo',
+    email: 'angelo@evan.com',
+    password: 'TSAEvanWord!!',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      {
+        title: 'JavaScript',
+        experience: 100,
+      },
+    ],
+  });
+
+  console.log(user?.name);
+
   return response.json({ message: 'Hello World' });
 }
