969576383bbdf7c24f9a15f8129233be1d8e87cb
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-14 21:56:11 -0300

    COnfigurando estrutura

---
+++ primeiro-projeto-node/src/server.ts
@@ -0,0 +1,11 @@
+import express, { request } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+  console.log('Server is running at http://localhost:3333 🚀');
+});
