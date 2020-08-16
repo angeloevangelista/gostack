1a66e4ac06f59349cddd5693519f6431d6cf859b
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-16 17:40:44 -0300

    Gerando Token JWT

--- iniciando-back-end/src/services/AuthenticateUserService.ts
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
-import { compare, hash } from 'bcryptjs';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,8 +34,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'ab98b2663ef3f81194792706b7825fc3', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
