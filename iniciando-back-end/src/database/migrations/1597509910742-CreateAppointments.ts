648e70319291df2e2b2f0deab723520ffe69071f
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-16 01:02:09 -0300

    Relacionando as entidades

--- iniciando-back-end/src/database/migrations/1597509910742-CreateAppointments.ts
+++ iniciando-back-end/src/database/migrations/1597509910742-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1597464445309
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- iniciando-back-end/src/models/Appointment.ts
+++ iniciando-back-end/src/models/Appointment.ts
@@ -4,7 +4,11 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -12,10 +16,14 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @CreateDateColumn()
   created_at: Date;
