f56d42b0b14911fc170a39380e713968bd7f5aab
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-18 16:52:50 -0300

    Servindo arquivos estáticos

--- iniciando-back-end/src/server.ts
+++ iniciando-back-end/src/server.ts
@@ -1,13 +1,17 @@
+import 'reflect-metadata';
+
+import { resolve } from 'path';
 import express from 'express';
 import routes from './routes';
 
-import 'reflect-metadata';
 import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/files', express.static(resolve(__dirname, '..', '..', 'tmp')));
 
 app.listen(3333, () => {
   console.log('Server is running at http://localhost:3333 🚀');
