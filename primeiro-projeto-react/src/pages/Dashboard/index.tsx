2cb7a9909b6d51175df7af47583d11f0a3a2ec5c
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-25 21:25:26 -0300

    Salvando em LocalStorage

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -19,7 +19,27 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [inputError, setInputError] = useState('');
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const [repositories, setRepositories] = useState<Repository[]>(
+    (): Repository[] => {
+      const storagedRepositories = localStorage.getItem(
+        '@GithubExplorer:repositories',
+      );
+
+      if (storagedRepositories) {
+        return JSON.parse(storagedRepositories);
+      }
+
+      return [];
+    },
+  );
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -31,6 +51,15 @@ const Dashboard: React.FC = () => {
 
     if (newRepo.split('/').length !== 2) {
       setInputError("Informe o repositório no formato 'autor/nome'");
+      return;
+    }
+
+    const repositoryAlreadySaved = repositories.find(
+      (repo) => repo.full_name === newRepo.toLowerCase().trim(),
+    );
+
+    if (repositoryAlreadySaved) {
+      setInputError('O repositório já está salvo');
       return;
     }
 
